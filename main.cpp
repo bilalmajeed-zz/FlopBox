18cd0c9eb046e7c655ff74db0d0ee4dd7a8444c6
Bilal Majeed <bilalmajeed247@gmail.com>
2014-07-09 14:10:19 -0400

    got rid of the console window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 // Flappy Bird Clone
 // By daloser247 aka Bilal Majeed
 
+//Define the minimum operating system for the application:
+#define _WIN32_WINNT _WIN32_WINNT_WINXP //Windows XP
+//Get rid of the annoying min() and max() macros:
+#define NOMINMAX
+//Include the windows header:
+#include <windows.h>
+
 #include "Game.h"
 
-int main()
+int wWinMain(HINSTANCE hInst, HINSTANCE prevInst, LPWSTR szCmdLine, int nCmdShow)
 {
 	Game game;
 	game.start();
